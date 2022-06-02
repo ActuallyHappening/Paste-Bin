7757cf17d96c43f5eff4c85506f06443514623f6
Smartguy88 - Smg88 <96052597+Smartestguy88@users.noreply.github.com>
2022-06-02 19:03:48 +1000

    Works and looks the same as original, debugs removed :)

--- horizon 3d/main.js
+++ horizon 3d/main.js
@@ -2,20 +2,14 @@ import '/style.css'
 import * as THREE from 'https://cdn.skypack.dev/three';
 import { TrackballControls } from 'https://cdn.skypack.dev/three@0.132.2/examples/jsm/controls/TrackballControls.js';
 
-
 const canvas = document.querySelector('.tiny-globe');
 const overlay = canvas.getContext('2d');
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
     55, window.innerWidth / window.innerHeight, 1, 1500
 );
-let debugCounter = 0;
 
-let quickAndDirtyDegreeCounter = 0;
-
-//const coolTestLinePoints = [ /* new THREE.Vector3(10, 10, 400), new THREE.Vector3(10, 10, -400) */ ]
-const coolLines = new THREE.Group() //new THREE.Line(new THREE.BufferGeometry().setFromPoints(coolTestLinePoints), new THREE.LineBasicMaterial({ color: 0xff0000 }));
-const cameraLines = new THREE.Group()
+let quickAndDirtyDegreeCounter = 0; /****************** NEW **********************/
 
 const projects = [{
         name: 'Center',
@@ -59,103 +53,76 @@ animate();
  * Initialise canvas
  */
 function init() {
-    window.addEventListener('resize', handleWindowResize, false);
+  window.addEventListener('resize', handleWindowResize, false);
 
-    /* const material = new THREE.LineBasicMaterial({
-        color: 0x0000ff
-    });
+  // Helpers
+  //scene.add(new THREE.GridHelper(1500, 100));
+  //scene.add(new THREE.AxesHelper(500));
 
-    const points = [];
-    points.push(new THREE.Vector3(-10, 0, 0));
-    points.push(new THREE.Vector3(0, 10, 0));
-    points.push(new THREE.Vector3(10, 0, 0));
+  // Camera
+  camera.position.x = 0;
+  camera.position.y = 10;
+  camera.position.z = 400;
 
-    const geometry = new THREE.BufferGeometry().setFromPoints(points);
+  // Renderer
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  canvas.insertAdjacentElement('afterend', renderer.domElement);
 
-    const line = new THREE.Line(geometry, material);
-    scene.add(line); */
+  // Planet object
+  // Group globe and markers together
+  // const loader = new GLTFLoader();
+  // loader.load(
+  // 	'https://communityportal.io/assets/objects/low-poly-planet.gltf',
+  // 	object => group.add(object.scene),
+  // 	xhr => console.log((xhr.loaded / xhr.total * 100 ) + '% loaded'),
+  // 	error => console.log('An error happened'),
+  // );
 
-    // Helpers
-    scene.add(new THREE.GridHelper(1500, 100));
-    scene.add(new THREE.AxesHelper(500));
+  // Lighting
+  sunlight = new THREE.DirectionalLight(0xf0fff0, 3.5);
+  sunlight.position.set(0, 0, 100);
 
-    // Camera
-    camera.position.x = 0;
-    camera.position.y = 10;
-    camera.position.z = 400;
+  // Controls
+  controls = new TrackballControls(camera, renderer.domElement);
+  controls.minDistance = 250;
+  controls.maxDistance = 1499;
+  controls.rotateSpeed = 1.0;
+  controls.zoomSpeed = 1.2;
+  controls.panSpeed = 0.8;
 
-    // Renderer
-    renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    canvas.insertAdjacentElement('afterend', renderer.domElement);
-
-    // Planet object
-    // Group globe and markers together
-    // const loader = new GLTFLoader();
-    // loader.load(
-    // 	'https://communityportal.io/assets/objects/low-poly-planet.gltf',
-    // 	object => group.add(object.scene),
-    // 	xhr => console.log((xhr.loaded / xhr.total * 100 ) + '% loaded'),
-    // 	error => console.log('An error happened'),
-    // );
-
-    // Lighting
-    sunlight = new THREE.DirectionalLight(0xf0fff0, 3.5);
-    sunlight.position.set(0, 0, 100);
-
-    // Controls
-    controls = new TrackballControls(camera, renderer.domElement);
-    controls.minDistance = 250;
-    controls.maxDistance = 1499;
-    controls.rotateSpeed = 1.0;
-    controls.zoomSpeed = 1.2;
-    controls.panSpeed = 0.8;
-
-    // Project markers
-    projects.forEach((project, index) => {
-        let html =
-            `<div class="marker" id="project-${ index + 1 }">
-				<a class="marker__label" href="${ project?.url }">
-					<strong>${ project.name }</strong>
+  // Project markers
+  projects.forEach((project, index) => {
+    let html =
+      `<div class="marker" id="project-${index + 1}">
+				<a class="marker__label" href="${project?.url}">
+					<strong>${project.name}</strong>
 				</a>
 				<div class="marker__pointer"></div>
 			</div>`;
 
-        let geometry = new THREE.SphereGeometry(10, 10, 10);
-        let material = new THREE.MeshPhysicalMaterial({ color: 0x0000ff });
-        let mesh = new THREE.Mesh(geometry, material);
+    let geometry = new THREE.SphereGeometry(10, 10, 10);
+    let material = new THREE.MeshPhysicalMaterial({ color: 0x0000ff });
+    let mesh = new THREE.Mesh(geometry, material);
 
-        mesh.position.set(
-            project.coords.x,
-            project.coords.y,
-            project.coords.z,
-        );
-        mesh.visible = true; // TODO was changed to true, set to false once finished
-        group.add(mesh);
+    mesh.position.set(
+      project.coords.x,
+      project.coords.y,
+      project.coords.z,
+    );
+    mesh.visible = false;
+    group.add(mesh);
 
-        canvas.insertAdjacentHTML('afterend', html);
-        project.element = document.querySelector(`#project-${ index + 1 }`);
-        project.mesh = mesh;
-    });
+    canvas.insertAdjacentHTML('afterend', html);
+    project.element = document.querySelector(`#project-${index + 1}`);
+    project.mesh = mesh;
+  });
 
-    markerPoints2D = document.querySelectorAll('.marker__pointer');
+  markerPoints2D = document.querySelectorAll('.marker__pointer');
 
-    scene.background = new THREE.Color(0x07131C);
-    scene.add(group, sunlight);
-
-    updateCoolLines();
-
-    scene.add(coolLines)
-    scene.add(cameraLines)
-}
-
-function updateCoolLines() {
-    const cameraLinesPoints = [
-        new THREE.Vector3(camera.position.x + 100, camera.position.y + 100, camera.position.z + 100),
-    ]
-    const cameraLinesGeometry = new THREE.BufferGeometry().setFromPoints(cameraLinesPoints);
-    cameraLines.add(new THREE.Line(cameraLinesGeometry, new THREE.LineBasicMaterial({ color: 0xff00f0 })));
+  scene.background = new THREE.Color(0x07131C);
+  scene.add(group, sunlight);
 }
 
 
@@ -163,23 +130,23 @@ function updateCoolLines() {
  * Updates animation frame.
  */
 function animate() {
-    zoomLevel = controls.target.distanceTo(controls.object.position); // UM i think the wrong line :)
+    zoomLevel = controls.target.distanceTo(controls.object.position);
     //console.log(controls.object.position) // 'Camera' position
   const axis = new THREE.Vector3(0, 1, 0) // Y axis to revolve around
   
+  /********************* NEW ****************** */
   group.children.forEach((projectRef, index) => {
-    const increase = 0.01
-    projectRef.position.applyAxisAngle(axis, increase);
-    quickAndDirtyDegreeCounter += increase;
+    const increase = 0.01; // Rotates by this amount
+    projectRef.position.applyAxisAngle(axis, increase); // Apply rotation to each individual project point
+    quickAndDirtyDegreeCounter += increase; // Also, to make calculating the label positions work, use this quick and dirty solution :)
   })
 
-    //group.rotation.y += 0.005; // TODO uncomment
-    //console.log(group);
+    //group.rotation.y += 0.005; // ********* OLD BUGGY LINE ************
 
     controls.update();
     renderer.render(scene, camera);
     //group.updateMatrixWorld; // Don't know what that was there!
-    updateCoolLines();
+    //updateCoolLines();
     updateMarkerSizes();
     updateMarkerPositions();
     requestAnimationFrame(animate);
@@ -222,9 +189,7 @@ function updateMarkerSizes() {
  * Update hotspot positions.
  */
 function updateMarkerPositions() {
-    const axis = new THREE.Vector3(0, 1, 0);
-
-    const coolLinesNow = [ /* new THREE.Vector3(10, 10, 400) */ ];
+    const axis = new THREE.Vector3(0, 1, 0); // Y axis
 
     projects.forEach((project, index) => {
 
@@ -233,15 +198,6 @@ function updateMarkerPositions() {
             project.coords.y,
             project.coords.z,
         );
-        const myCoolLinePoints = [];
-        //console.log(camera.position);
-        myCoolLinePoints.push(new THREE.Vector3(0, 0, 0));
-        //myCoolLinePoints.push(camera.position);
-        myCoolLinePoints.push(new THREE.Vector3(0, 0, 0));
-        //myCoolLinePoints.push(vector.clone());
-        myCoolLinePoints.push(new THREE.Vector3(0, 0, 0))
-
-        //console.log(myLineGeometry);
 
       vector.applyAxisAngle(axis, quickAndDirtyDegreeCounter / (5)); // BLACK MAGIC WT* I HAVE GENUINELY NO IDEA WHY THIS WORKS!
         // Any workaround is going to have to manually compute above line! TODO
@@ -253,43 +209,17 @@ function updateMarkerPositions() {
         project.element.style.top = `${vector.y}px`;
         project.element.style.left = `${vector.x}px`;
 
+      
+          /* **** Only Refactored ***** */
         // Track if items move behind globe
-        // Distance to center
         const distanceToCenter = camera.position.distanceTo(new THREE.Vector3(0, 0, 0));
         const distanceToProject = camera.position.distanceTo(project.mesh.position);
-        const distanceToProjectT = camera.position.distanceTo(group.children[index].position);
-        let should = false;
-        debugCounter += 1;
-        if (debugCounter % 200 == 69) {
-            console.table({ "name": project.name, "distanceToCenter": distanceToCenter, "distanceToProject": distanceToProject, "should": should, "distanceToProjectT": distanceToProjectT, "Position": `(x: ${project.mesh.position.x}, y: ${project.mesh.position.y}, z: ${project.mesh.position.z})` });
-        }
-        if (debugCounter % 500 == 69) {
-            //console.log(group)
-        }
+        let should = false; // Whether the project point is behind or not
         if (distanceToProject > distanceToCenter) { should = true; }
         project.element.classList.toggle(
             'is-behind',
             should
         );
-
-        myCoolLinePoints.push(project.mesh.position);
-        //myCoolLinePoints.push(vector);
-        myCoolLinePoints.push(new THREE.Vector3(0, 0, 0));
-        myCoolLinePoints.push(group.children[index].position);
-        myCoolLinePoints.push(new THREE.Vector3(0, 0, 0));
-        //myCoolLinePoints.push(controls.object.position);
-
-        const myLineGeometry = new THREE.BufferGeometry().setFromPoints(myCoolLinePoints);
-        if (coolLines.children[index] == undefined) {
-            coolLines.children[index] = new THREE.Line(myLineGeometry, new THREE.LineBasicMaterial({ color: 0xff0000 }));
-        }
-        coolLines.children[index].geometry = myLineGeometry; // Updates my coolLines TODO remove!
-
-        //scene.add(vector);
         //console.log(project.point.position);
     });
-
-    //coolLines.children = coolLinesNow
-
-    //console.log(coolLines);
 }
