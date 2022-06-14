46a1153e05ccfd108650278e46cd7388adbddff9
Smartguy88 - Smg88 <96052597+Smartestguy88@users.noreply.github.com>
2022-06-14 18:19:47 +1000

    Still trying to get HTMl to work :)

--- RefactorAttemptReact1/src/App.tsx
+++ RefactorAttemptReact1/src/App.tsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
+import QAD_Structure from './components/QAD_Structure'
+import QAD_Three from './components/QAD_Three'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-       
+      <QAD_Structure />
+      <QAD_Three/>
     </div>
   )
 }
---
+++ RefactorAttemptReact1/src/components/QAD_Structure.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const QAD_Structure = () => {
+  return (
+    <>
+    <canvas className="tiny-globe"></canvas>
+
+<div className="panel" id="about">
+	<div className="panel__content">
+		<a className="panel__close" href="/#">
+			<span className="material-icons">&#xe5cd;</span>
+		</a>
+		<p>
+			Welcome to communityportal. This is the gateway to all your community engagement needs. Here. Yes, here.
+		</p>
+		<p>
+			If you can't find it, tell us. We'll build it for you.
+		</p>
+		<p>
+			Batteries included.
+		</p>
+	</div>
+</div>
+
+<div className="panel" id="contact">
+	<div className="panel__content">
+		<a className="panel__close" href="/#">
+			<span className="material-icons">&#xe5cd;</span>
+		</a>
+		<p>
+			For all enquiries, please reach out to David.
+		</p>
+		<p>
+			<strong>David Sweeney</strong>
+			<br/><a href="mailto:David.Sweeney@aurecongroup.com">David.Sweeney@aurecongroup.com</a>
+			<br/>Director, Communication and Stakeholder Engagement,
+			<br/>Aurecon
+		</p>
+        </div>
+        </div>
+    </>
+  )
+}
+
+export default QAD_Structure
