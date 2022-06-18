63588e0fc902da2ed06d7f18d480e9bcca371428
Smartguy88 - Smg88 <96052597+Smartestguy88@users.noreply.github.com>
2022-06-18 13:39:26 +1000

    Cleaning up

--- Refactor Attempt 2/src/App.tsx
+++ Refactor Attempt 2/src/App.tsx
@@ -1,8 +1,4 @@
-import { useState } from 'react'
 import Body from './components/body'
-import QAD_Body from './components/QAD_Body'
-import QAD_Body2 from './components/QAD_Body2'
-import QAD_Three from './components/three/QAD_Three'
 import './styles.main.dev.css'
 
 function AppFullscreen() {
