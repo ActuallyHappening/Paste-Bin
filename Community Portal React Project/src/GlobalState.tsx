3cc3ff9befac9d093231e3ca04acc1ed57939cd0
Smartguy88 - Smg88 <96052597+Smartestguy88@users.noreply.github.com>
2022-06-21 18:14:19 +1000

    Going to get panels to work ...

--- Community Portal React Project/src/GlobalState.tsx
+++ Community Portal React Project/src/GlobalState.tsx
@@ -7,7 +7,9 @@ export type T_GlobalState = {projects: Array<Project>, items: Array<MenuItem>}
 
 const _G: T_GlobalState = {
   "projects": [new Project({id: 1, shortDescription: "Testing Project (SD)", longDescription: "Testing Project (LD)", name: "Testing Project (N)"})],
-  "items": [new MenuItem({purposeType: "meta", name: "About", shortDescription: "About"})],
+  "items": [
+    new MenuItem({purposeType: "meta", name: "About", shortDescription: "About", url: "/#about"}),
+    new MenuItem({purposeType: "meta", name: "Contact", shortDescription: "Contact", url: "/#contact"})],
 }
 
 _G.projects.forEach((project, index) => {
