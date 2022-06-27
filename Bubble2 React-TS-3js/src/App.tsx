489561b8c43f2d895965fdcdb8195130bc88c00d
Smartguy88 - Smg88 <96052597+Smartestguy88@users.noreply.github.com>
2022-06-27 10:54:25 +1000

    added app again!

---
+++ Bubble2 React-TS-3js/src/App.tsx
@@ -0,0 +1,12 @@
+import Body from './components/body'
+
+function App() {
+  return (
+    <div className='App'>
+      <Body/>
+      {/* This is where the loading screen could be implemented */}
+    </div>
+  )
+}
+
+export default App
