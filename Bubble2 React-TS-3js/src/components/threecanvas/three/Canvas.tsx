1e24a6b440f79da26cbc81ac768fb8f767193c78
Smartguy88 - Smg88 <96052597+Smartestguy88@users.noreply.github.com>
2022-06-27 15:10:31 +1000

    ok it is time to make this poc

--- Bubble2 React-TS-3js/src/components/threecanvas/three/Canvas.tsx
+++ Bubble2 React-TS-3js/src/components/threecanvas/three/Canvas.tsx
@@ -1,22 +1,33 @@
 import { OrbitControls } from '@react-three/drei'
-import { Canvas } from '@react-three/fiber'
-import { T_HelperOptions } from '../../styles/LayoutHelper'
-import Engine from '../../../assets/GLTF_Prebuilt_Modules/Engine'
+import { Canvas, useThree } from '@react-three/fiber'
 
-const canvas = ({children}: {children?: React.ReactNode}) => {
+const THREECanvas = ({children}: {children?: React.ReactNode}) => {
   return (
-    <Canvas>
-      <ambientLight intensity={0.1} />
-      <directionalLight color="red" position={[0, 0, 5]} />
+    <Canvas
+      camera={{position: [0, 10, 0], fov:55, far:1000}}
+    >
+      <directionalLight color="red" position={[0, 0, 100]} intensity={4}/>
       <OrbitControls />
       <mesh>
-        <boxGeometry />
-        <meshStandardMaterial />
+        <boxGeometry args={[100, 5, 100]}/>
+        <meshBasicMaterial color="green"/>
       </mesh>
-      <Engine />
+      <gridHelper args={[1500, 100]}/>
+      <axesHelper args={[500]}/>
       {children}
+      <CameraHelper />
     </Canvas>
   )
 }
 
-export default canvas
+const deg2rad = (degrees: number) => degrees * (Math.PI / 180)
+
+const CameraHelper = () => {
+  useThree(({camera}) => {
+    console.log("camera", camera)
+    camera?.rotation.set(deg2rad(90), 0, 0)
+  })
+  return <></>
+}
+
+export default THREECanvas
