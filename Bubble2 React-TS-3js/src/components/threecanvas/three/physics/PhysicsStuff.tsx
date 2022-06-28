4e5c6177cf5b8e77c8decf5a6a276d160f7dd10b
Smartguy88 - Smg88 <96052597+Smartestguy88@users.noreply.github.com>
2022-06-28 21:23:49 +1000

    YES thats it, got a point event intersection working :)

--- Bubble2 React-TS-3js/src/components/threecanvas/three/physics/PhysicsStuff.tsx
+++ Bubble2 React-TS-3js/src/components/threecanvas/three/physics/PhysicsStuff.tsx
@@ -1,19 +1,34 @@
 import { Debug, Physics, useBox, usePlane } from '@react-three/cannon'
-import React from 'react'
+import { Point, Points } from '@react-three/drei'
+import React, { useRef, useState } from 'react'
 import { useEffect } from 'react'
 import Player from './Player'
 
 const π = Math.PI
 
 function MyPlane({...props}) {
+  const pointRef = useRef()
+  const [debugPos, setDebugPos] = useState([0, 0, 0])
   const [ref, api] = usePlane(() => ({ rotation: [-π / 2, 0, 0], ...props }))
-  useEffect(() => {
-    api.applyForce([0, 0, -10 ], [0, 1, 0])
-  })
   return (
-    <mesh ref={ref}>
+    <>
+    <Points>
+      <pointsMaterial vertexColors />
+      <Point ref={pointRef} color="red" position={debugPos}/>
+    </Points>
+    <mesh
+      ref={ref}
+      onPointerMove={e => {
+        Math.random() < 0.1 ? console.log(e) : null
+        const intersectOnPlanePoint = e.intersections[0].point
+        console.log(intersectOnPlanePoint)
+        setDebugPos(intersectOnPlanePoint)
+        //api.applyLocalForce(intersectOnPlanePoint.toArray(), [0, 0, 0])
+      }}
+      >
       <planeGeometry args={[100, 100]} />
     </mesh>
+      </>
   )
 }
 
@@ -21,8 +36,8 @@ const PhysicsStuff = () => {
   return (
     <Physics>
       <Debug color="black" scale={1.1}>
-        <Player />
         <MyPlane />
+        <Player />
       </Debug>
     </Physics>
   )
--- Bubble2 React-TS-3js/src/components/threecanvas/three/physics/Player.tsx
+++ Bubble2 React-TS-3js/src/components/threecanvas/three/physics/Player.tsx
@@ -1,13 +1,17 @@
 import { useBox, usePlane } from '@react-three/cannon'
 import { useCursor } from '@react-three/drei'
+import { useFrame } from '@react-three/fiber'
 import { useState } from 'react'
 
 
 
 const Player = () => {
   const [hovered, setHovered] = useState(false)
-  const [boxRef] = useBox(() => ({mass: 1, position: [0, 5, 0]}))
+  const [boxRef, api] = useBox(() => ({mass: 1, position: [0, 5, 0]}))
   useCursor(hovered) /* Sets cursor to grabbing */
+  useFrame(() => {
+    api.applyLocalForce([0, 0, 0 ], [0, 0, 0])
+  })
   return (
     <>
     <mesh
